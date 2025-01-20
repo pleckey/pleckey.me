892777449e351913c3747c538d5d54b6f71eebb1
Patrick Leckey <pat.leckey@gmail.com>
2025-01-20 15:21:43 -0500

    add cv

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -11,6 +11,18 @@
   <div class="container">
     <header class="header">
       <a class="header__title" href="/">Patrick Leckey</a>
+      <div class="header__social-container">
+        <a class="header__social" href="https://linkedin.com/in/patrickleckey" target="_blank" rel="noopener">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+          </svg>
+        </a>
+        <a class="header__social" href="/pleckey-cv.pdf" target="_blank" rel="noopener">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm4 18H6V4h7v5h5v11zM8 15h8v2H8v-2zm0-4h8v2H8v-2z"/>
+          </svg>
+        </a>
+      </div>
     </header>
     <main>
       <NuxtPage />
@@ -124,6 +136,8 @@ main {
 
 .header {
   display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding: 0 var(--space-1);
   border-bottom: 1px solid #ced2d9;
 
@@ -227,5 +241,19 @@ main {
 }
 .preview-toggle:hover span:last-child {
   display: block;
+}
+
+.header__social-container {
+  display: flex;
+  gap: var(--space-2);
+  align-items: center;
+}
+
+.header__social {
+  color: var(--text);
+}
+
+.header__social:hover {
+  color: var(--highlight);
 }
 </style>
