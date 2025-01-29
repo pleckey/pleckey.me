49a5aae826f93dde9f30b21133a4e113fcfa75d0
Patrick Leckey <pat.leckey@gmail.com>
2025-01-29 14:48:27 -0500

    add typing to post queries

--- nuxt-app/server/api/feed.ts
+++ nuxt-app/server/api/feed.ts
@@ -1,6 +1,6 @@
 // server/routes/rss.ts
 import RSS from 'rss';
-import { createClient } from '@sanity/client';
+import { type Post } from '~/../studio/sanity.types';
 
 export default defineEventHandler(async (event) => {
     try {
@@ -9,21 +9,16 @@ export default defineEventHandler(async (event) => {
         const feed = new RSS({
             title: 'Patrick Leckey',
             site_url: 'https://pleckey.me', // link to your website/blog
-            feed_url: `https://pleckey.me/feed`, // path to your rss feed
+            feed_url: 'https://pleckey.me/feed', // path to your rss feed
         });
-        
+
         // fetch data from dev.to
         const query = '*[ _type == "post" && defined(slug.current) ] | order(_createdAt desc)';
-        const sanity = createClient({
-            projectId: config.public.sanity.projectId,
-            dataset: config.public.sanity.dataset,
-            apiVersion: config.public.sanity.apiVersion,
-            useCdn: true,
-        });
-        const blogposts = await sanity.fetch(query);
+        const client = useSanity().client;
+        const blogposts = await client.fetch<Post[]>(query);
 
         if (blogposts) {
-            blogposts.forEach((post: any) => {
+            blogposts.map((post: any) => {
                 feed.item({
                     title: post.title || 'Untitled Post',
                     url: `https://pleckey.me/post/${post.slug?.current || ''}`,
--- nuxt-app/server/api/posts.ts
+++ nuxt-app/server/api/posts.ts
@@ -1,15 +1,15 @@
-import { defineSitemapEventHandler } from '#imports'
-import type { SitemapUrlInput } from '#sitemap/types'
+import { defineSitemapEventHandler } from '#imports';
+import type { SitemapUrlInput } from '#sitemap/types';
+import { type Post } from '~/../studio/sanity.types';
 
-// server/api/__sitemap__/urls.ts
 export default defineSitemapEventHandler(async () => {
     const client = useSanity().client;
     const query = '*[_type == "post"] { slug, _updatedAt }';
-    const blogposts = await client.fetch(query);
+    const blogposts = await client.fetch<Post[]>(query);
     return blogposts.map((post: any) => ({
         loc: `/post/${post.slug.current}`,
         lastmod: post._updatedAt,
         changefreq: 'weekly',
         priority: 0.8,
     }) satisfies SitemapUrlInput[]);
-})
+});
